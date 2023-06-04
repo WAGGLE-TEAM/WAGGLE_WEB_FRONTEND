94ffdd016930eff983cd8f78f4e404f4ad6bafa5
yu-heejin <jkgt0521@naver.com>
2023-06-04 22:13:55 +0900

    feat: jest config 설정 추가 [#10]

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
       "**/?(*.)+(spec|test).+(ts|tsx|js)"
     ],
     "transform": {
-      "^.+\\.(ts|tsx)$": "ts-jest"
+      "^.+\\.(ts|tsx)$": "babel-jest"
     },
+    "testEnvironment": "jsdom",
   }
