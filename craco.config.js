bcd9b137b8dacc5bc31b539e692afd134638f05c
sylee <sylee6529@gmail.com>
2023-07-30 14:24:04 +0900

    feat: module alias 설정  [#18]

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+export const plugins = [
+    {
+        plugin: {
+            overrideWebpackConfig: ({ webpackConfig }) => {
+                webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
+                return webpackConfig;
+            }
+        }
+    }
+];
